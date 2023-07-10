7f25c6cfbe901abfe769b04f45954092f577665f
Ali Mahdavi <a5mahdavy5@gmail.com>
2023-07-10 11:37:50 +0200

    update hardhat info

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
+import dotenv from 'dotenv';
 import "@nomicfoundation/hardhat-toolbox";
 
+dotenv.config()
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.18",
+  networks: {
+    mumbai: {
+      url: process.env.MUMBAI_RPC,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
