d73ee800d0e2ac041153070c22e783c4bcd53712
Ali Mahdavi <a5mahdavy5@gmail.com>
2023-07-28 10:24:45 +0200

    add gitsubmodule for ERC6551

--- contracts/TBA.sol
+++ contracts/TBA.sol
@@ -1,113 +1,113 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.7.0 <0.9.0;
 
-// import "@thirdweb-dev/contracts/smart-wallet/non-upgradeable/Account.sol";
-// import "@thirdweb-dev/contracts/eip/interface/IERC721.sol";
-// import "https://github.com/erc6551/reference/blob/v0.2.0/src/lib/ERC6551AccountLib.sol";
-// import "https://github.com/erc6551/reference/blob/v0.2.0/src/interfaces/IERC6551Account.sol";
+import "@thirdweb-dev/contracts/smart-wallet/non-upgradeable/Account.sol";
+import "@thirdweb-dev/contracts/eip/interface/IERC721.sol";
+import "./reference/src/lib/ERC6551AccountLib.sol";
+import "./reference/src/interfaces/IERC6551Account.sol";
 
-// contract TokenBoundAccount is Account, IERC6551Account {
-//     /*///////////////////////////////////////////////////////////////
-//                             Events
-//     //////////////////////////////////////////////////////////////*/
+contract TokenBoundAccount is Account, IERC6551Account {
+    /*///////////////////////////////////////////////////////////////
+                            Events
+    //////////////////////////////////////////////////////////////*/
 
-//     event TokenBoundAccountCreated(address indexed account, bytes indexed data);
+    event TokenBoundAccountCreated(address indexed account, bytes indexed data);
 
-//     /*///////////////////////////////////////////////////////////////
-//                             Constructor
-//     //////////////////////////////////////////////////////////////*/
+    /*///////////////////////////////////////////////////////////////
+                            Constructor
+    //////////////////////////////////////////////////////////////*/
 
-//     /**
-//      * @notice Executes once when a contract is created to initialize state variables
-//      *
-//      * @param _entrypoint - 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789
-//      * @param _factory - The factory contract address to issue token Bound accounts
-//      *
-//      */
-//     constructor(
-//         IEntryPoint _entrypoint,
-//         address _factory
-//     ) Account(_entrypoint, _factory) {
-//         _disableInitializers();
-//     }
+    /**
+     * @notice Executes once when a contract is created to initialize state variables
+     *
+     * @param _entrypoint - 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789
+     * @param _factory - The factory contract address to issue token Bound accounts
+     *
+     */
+    constructor(
+        IEntryPoint _entrypoint,
+        address _factory
+    ) Account(_entrypoint, _factory) {
+        _disableInitializers();
+    }
 
-//     receive() external payable override(IERC6551Account, Account) {}
+    receive() external payable override(IERC6551Account, Account) {}
 
-//     /// @notice Returns whether a signer is authorized to perform transactions using the wallet.
-//     function isValidSigner(
-//         address _signer
-//     ) public view override returns (bool) {
-//         return (owner() == _signer);
-//     }
+    /// @notice Returns whether a signer is authorized to perform transactions using the wallet.
+    function isValidSigner(
+        address _signer
+    ) public view override returns (bool) {
+        return (owner() == _signer);
+    }
 
-//     function owner() public view returns (address) {
-//         (
-//             uint256 chainId,
-//             address tokenContract,
-//             uint256 tokenId
-//         ) = ERC6551AccountLib.token();
+    function owner() public view returns (address) {
+        (
+            uint256 chainId,
+            address tokenContract,
+            uint256 tokenId
+        ) = ERC6551AccountLib.token();
 
-//         if (chainId != block.chainid) return address(0);
+        if (chainId != block.chainid) return address(0);
 
-//         return IERC721(tokenContract).ownerOf(tokenId);
-//     }
+        return IERC721(tokenContract).ownerOf(tokenId);
+    }
 
-//     function executeCall(
-//         address to,
-//         uint256 value,
-//         bytes calldata data
-//     ) external payable onlyAdminOrEntrypoint returns (bytes memory result) {
-//         return _call(to, value, data);
-//     }
+    function executeCall(
+        address to,
+        uint256 value,
+        bytes calldata data
+    ) external payable onlyAdminOrEntrypoint returns (bytes memory result) {
+        return _call(to, value, data);
+    }
 
-//     /// @notice Withdraw funds for this account from Entrypoint.
-//     function withdrawDepositTo(
-//         address payable withdrawAddress,
-//         uint256 amount
-//     ) public virtual override {
-//         require(owner() == msg.sender, "Account: not NFT owner");
-//         entryPoint().withdrawTo(withdrawAddress, amount);
-//     }
+    /// @notice Withdraw funds for this account from Entrypoint.
+    function withdrawDepositTo(
+        address payable withdrawAddress,
+        uint256 amount
+    ) public virtual override {
+        require(owner() == msg.sender, "Account: not NFT owner");
+        entryPoint().withdrawTo(withdrawAddress, amount);
+    }
 
-//     function token()
-//         external
-//         view
-//         returns (uint256 chainId, address tokenContract, uint256 tokenId)
-//     {
-//         return ERC6551AccountLib.token();
-//     }
+    function token()
+        external
+        view
+        returns (uint256 chainId, address tokenContract, uint256 tokenId)
+    {
+        return ERC6551AccountLib.token();
+    }
 
-//     function nonce() external view returns (uint256) {
-//         return getNonce();
-//     }
+    function nonce() external view returns (uint256) {
+        return getNonce();
+    }
 
-//     /*///////////////////////////////////////////////////////////////
-//                             Internal Functions
-//     //////////////////////////////////////////////////////////////*/
+    /*///////////////////////////////////////////////////////////////
+                            Internal Functions
+    //////////////////////////////////////////////////////////////*/
 
-//     function _call(
-//         address _target,
-//         uint256 value,
-//         bytes memory _calldata
-//     ) internal virtual override returns (bytes memory result) {
-//         bool success;
-//         (success, result) = _target.call{value: value}(_calldata);
-//         if (!success) {
-//             assembly {
-//                 revert(add(result, 32), mload(result))
-//             }
-//         }
-//     }
+    function _call(
+        address _target,
+        uint256 value,
+        bytes memory _calldata
+    ) internal virtual override returns (bytes memory result) {
+        bool success;
+        (success, result) = _target.call{value: value}(_calldata);
+        if (!success) {
+            assembly {
+                revert(add(result, 32), mload(result))
+            }
+        }
+    }
 
-//     /*///////////////////////////////////////////////////////////////
-//                             Modifiers
-//     //////////////////////////////////////////////////////////////*/
+    /*///////////////////////////////////////////////////////////////
+                            Modifiers
+    //////////////////////////////////////////////////////////////*/
 
-//     modifier onlyAdminOrEntrypoint() override {
-//         require(
-//             msg.sender == address(entryPoint()) || msg.sender == owner(),
-//             "Account: not admin or EntryPoint."
-//         );
-//         _;
-//     }
-// }
+    modifier onlyAdminOrEntrypoint() override {
+        require(
+            msg.sender == address(entryPoint()) || msg.sender == owner(),
+            "Account: not admin or EntryPoint."
+        );
+        _;
+    }
+}
